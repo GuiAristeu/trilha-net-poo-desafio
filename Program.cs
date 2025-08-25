d4b1e8f800c9ac85879b6217328351bab8a5ab34
Gui Aristeu <guilhermearisteu@gmail.com>
2025-08-25 18:55:38 -0300

    desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone1 = new Iphone("11-99999-9999", "XR", "123712831", 256);
+Smartphone nokia1 = new Nokia("11-98888-98888", "Lumia", "423423423", 128);
+
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Instagram");
+
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("WhatsApp");
